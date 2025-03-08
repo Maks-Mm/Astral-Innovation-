53baea5259d6e6698085418e07102d3079a63557
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-03-08 18:05:00 +0100

    iit is added a card to javascript

--- src/components/guides/JavaScript.js
+++ src/components/guides/JavaScript.js
@@ -5,7 +5,19 @@ function JavaScript() {
     <div className='sub-page'>
 
       <div className='sub-page-card  text-white'>
-        JavaScript
+        <h2> this is a tutorial with react for lerner </h2>
+        <p></p>
+        <iframe
+          height="300"
+          style={{ width: "100%" }}
+          title="Javascript Utility functions"
+          src="https://codepen.io/Zorlimar/embed/vQQmOo?default-tab="
+          frameBorder="no"
+          loading="lazy"
+          allowTransparency="true"
+          allowFullScreen="true"
+          className="rounded-lg"
+        ></iframe>
       </div>
       <div className='sub-page-card  text-white'>
         JavaScript
