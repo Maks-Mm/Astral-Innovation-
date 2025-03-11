37d9967707339a2f8b9224cd152123b4da1dbf30
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-03-11 21:07:47 +0100

    photos with array

--- src/components/guides/GuidStart.js
+++ src/components/guides/GuidStart.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './GuidStart.css'; // Import the CSS file
 
 export default function GuidStart() {
+  // Array containing image URLs
+  const imageSources = [
+    "JavaScript.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    "https://cdn-icons-png.flaticon.com/512/8167/8167751.png",
+    
+    
+  ];
+
   return (
     <div className='sub-page'>
-
-      <div className='sub-page-card  text-white'>
+      <div className='sub-page-card text-white'>
         info that helps at starting
-
       </div>
-<img src="https://cdn-icons-png.flaticon.com/512/8167/8167751.png" alt="img" className="sub-page-img" />
+      {/* Use map to render images from the array */}
+      {imageSources.map((src, index) => (
+        <img key={index} src={src} alt={`img-${index + 1}`} className="sub-page-img" />
+      ))}
     </div>
-
-  )
+  );
 }
