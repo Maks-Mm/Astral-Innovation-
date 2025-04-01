daa78b528ca6d7da9cb23fda52f327970c4b1a50
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-04-01 21:51:11 +0200

    place for Next is developing

--- src/components/guides/Next.js
+++ src/components/guides/Next.js
@@ -1,23 +1,16 @@
 import React from 'react'
+import './Next.css'
 
 function Next() {
-    return (
-        <div className='sub-page'>
+  return (
+    <div className='sub-page'>
 
-      <div className='sub-page-card  text-white screen '>
-        JavaScript
+      <div className='tv-effect sub-page-card text-white'>
+        Next.js is a powerful React framework that enables developers to build server-rendered applications with ease. It provides a set of features and conventions that simplify the development process. Here’s an overview of its structure, key features, and some important points:
       </div>
-      <div className='sub-page-card  text-white screen'>
-        JavaScript
-      </div>
-      <div className='sub-page-card  text-white screen'>
-        JavaScript
-      </div>
-      <div className='sub-page-card  text-white screen'>
-        JavaScript
-      </div>
+     
     </div>
-    )
+  )
 }
 
 export default Next
