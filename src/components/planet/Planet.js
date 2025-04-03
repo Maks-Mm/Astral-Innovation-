d63c5ec04cca5e8c0808cc2b4d9bd618b83c0512
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-04-03 23:39:40 +0200

    new position to planet

--- src/components/planet/Planet.js
+++ src/components/planet/Planet.js
@@ -3,7 +3,7 @@ import "./Planet.css";
 
 const Planet = () => {
   return (
-    <div className="wrapper">
+    <div className="planet-position">
       <div className="earth day"></div>
       <div className="earth night"></div>
     </div>
