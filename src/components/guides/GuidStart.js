8a32719f884a61df514f7efc149b4d7972566bd6
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-04-08 21:20:12 +0200

    improvisation with a tags and links to infos

--- src/components/guides/GuidStart.js
+++ src/components/guides/GuidStart.js
@@ -1,39 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import './GuidStart.css'; // Import the CSS file
 
-
 export default function GuidStart() {
-  // Array containing image URLs
+  // Array containing image objects with src and link
   const imageSources = [
-    "https://static-00.iconduck.com/assets.00/logo-js-icon-2048x2048-ptuzd8a3.png",
-    "icons8-nuxt-js-48.png",
-    "icons8-express-js-64.png",
-    "/icons8-adonis-js-48.png",
-    "icons8-nodejs-48.png", "icons8-nuxt-js-48.png",
-    "icons8-express-js-64.png",
-    "/icons8-adonis-js-48.png",
-    "icons8-nodejs-48.png",
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fweb.dev%2Fcss%3Fhl%3Dde&psig=AOvVaw3Dxbxz4HXik0I9nF7Hhv_5&ust=1742682928033000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNi6gfmdnIwDFQAAAAAdAAAAABAE",
-
-
+    {
+      src: "https://static-00.iconduck.com/assets.00/logo-js-icon-2048x2048-ptuzd8a3.png",
+      link: "https://developer.mozilla.org/ru/docs/Learn_web_development/Core/Scripting/What_is_JavaScript"
+    },
+    {
+      src: "/icons8-nuxt-js-48.png", // Correct path
+      link: "https://en.wikipedia.org/wiki/Nuxt"
+    },
+    {
+      src: "/icons8-express-js-64.png", // Correct path
+      link: "https://www.codecademy.com/article/what-is-express-js",
+    },
+    {
+      src: "/icons8-adonis-js-48.png", // Correct path
+      link: "https://dev.to/tngeene/introduction-to-adonisjs-4a1f",
+    },
+    {
+      src: "/icons8-nodejs-48.png", // Correct path
+      link: "https://nodejs.org/en/about",
+    },
+    {
+      src: "/icons8-nuxt-js-48.png", // Correct path
+      link: "https://nuxt.com/docs/examples/hello-world"
+    },
+    {
+      src: "/icons8-express-js-64.png", // Correct path
+      link: "https://expressjs.com/en/starter/examples.html"
+    },
+    {
+      src: "/icons8-adonis-js-48.png", // Correct path
+      link: "https://docs.adonisjs.com/guides/preface/introduction"
+    },
+    
   ];
-
 
   const text = "Welcome to the guides section! Here, you'll find not only detailed instructions on JavaScript, React, Node.js, and other technologies but also support from like-minded individuals. Our guides will help you understand the intricacies and inspire you to take on new projects. Join our community, share your experiences, and grow with us!";
 
   const [screeText, setScreeText] = useState('');
-
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setScreeText((prev) => prev + text[index]);
-      console.log(index);
-      if (index > text.length - 1) {
+      if (index >= text.length - 1) {
         clearInterval(interval);
       } else {
         setIndex((prev) => (prev + 1));
-
       }
     }, 30);
     return () => clearInterval(interval);
@@ -41,14 +58,16 @@ export default function GuidStart() {
 
   return (
     <div className='sub-page'>
-      <div className='sub-page-card text-white screen' >
-        <div className='substrate' >
+      <div className='sub-page-card text-white screen'>
+        <div className='substrate'>
           {screeText} <span className='cursor blinking-text'>_</span>
         </div>
       </div>
-      {/* Use map to render images from the array */}
-      {imageSources.map((src, index) => (
-        <img key={index} src={src} alt={`img-${index + 1}`} className="sub-page-img" />
+      {/* Use map to render images from the array without linking */}
+      {imageSources.map((image, index) => (
+        <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+          <img key={index} src={image.src} alt={`img-${index + 1}`} className="sub-page-img" />
+        </a>
       ))}
     </div>
   );
