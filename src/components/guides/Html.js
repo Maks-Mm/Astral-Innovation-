0b7a1f7db62f9c647c49111bd4521485e56c6e5f
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-04-13 20:16:29 +0200

    html is deleted

--- src/components/guides/Html.js
+++ src/components/guides/Html.js
@@ -5,13 +5,14 @@ export default function Html() {
   // Array containing image URLs
   const imageSources = [
     "/html.png",
-    "https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-icons-download-14.png"
   ];
 
   return (
     <div className='sub-page'>
       <div className='sub-page-card text-white screen'>
-        Info that helps at starting
+      Hypertext Markup Language (HTML) is the standard markup language[a] for documents designed to be displayed in a web browser. It defines the content and structure of web content. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript, a programming language.
+
+Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for its appearance. 
       </div>
       {/* Use map to render images from the array */}
       {imageSources.map((src, index) => (
