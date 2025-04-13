f8f1e508f5ee02dc118680ad66c6877c13d2b447
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2025-04-13 16:58:38 +0200

    new order to cards placing

--- src/components/guides/Next.js
+++ src/components/guides/Next.js
@@ -6,47 +6,26 @@ function Next() {
         <div className='sub-page relative'>
             <div className='tv-effect card-css '>
             </div>
+            <div className='tv-effect2 card-css '>
+            </div>
+            <div className='tv-effect3 card-css '>
+            </div>
+            <div className='tv-effect4 card-css '>
+            </div>
+            <div className='tv-effect5 card-css '>
+            </div>
+            <div className='tv-effect6 card-css '>
+            </div>
+
 
             <div className='image-container z-20 mt-10'>
                 <div className='top-image'>
-                   
+
                 </div>
-                {/* First Row: Three Images */}
-                <div className='image-row'>
-                    <img
-                        src="/TypeScript.png"
-                        alt="TypeScript"
-                        className='image-small w-80 h-70 object-cover'
-                    />
-                    <img
-                        src="/Optimizing.jpeg"
-                        alt="Optimizing"
-                        className='image-small w-80 h-70 object-cover'
-                    />
-                    <img
-                        src="/ChatGPTStylingNext.png"
-                        alt="ChatGPT Styling Next"
-                        className='image-small w-80 h-70 object-cover'
-                    />
-                </div>
-                {/* Second Row: Two Images */}
-                <div className='image-row'>
-                    <img
-                        src="/NextRouting.jpg"
-                        alt="Next Routing"
-                        className='image-small w-80 h-70 object-cover'
-                    />
-                    <img
-                        src="/RenderingNext.jpg"
-                        alt="Rendering Next"
-                        className='image-small w-80 h-70 object-cover'
-                    />
-                </div>
+
             </div>
-            <div className='text-white'>
-                Idea to create new tutorials structure of Next.js
-            </div>
-            <div className='text-white'>
+
+            <div className=''>
                 Documentation that helps developers get started and troubleshoot issues effectively.
             </div>
         </div>
